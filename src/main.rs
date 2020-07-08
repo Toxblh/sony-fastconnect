063e32febb1fb894bece35bada933ed4cc3e0bf3
Anton Palgunov <toxblh@gmail.com>
2020-07-08 14:44:02 +0100

    Able to disconnect

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use blurz::BluetoothSession;
 use blurz::BluetoothDevice;
+use blurz::BluetoothSession;
+use clap::{App, Arg};
 
 fn main() {
+    let matches = App::new("sony-fastconnect")
+        .version("1.0")
+        .author("Anton Palgunov <toxblh@gmail.com>")
+        .about("Fast connect Sony headphones")
+        .arg(
+            Arg::new("disconnect")
+                .about("For fast disconnect")
+                .short('d')
+        )
+        .get_matches();
+
     let path = "/org/bluez/hci0/dev_70_26_05_10_48_04";
 
     let session = BluetoothSession::create_session(None).unwrap();
     let device = BluetoothDevice::new(&session, path.to_string());
 
-    match device.connect(2_000) {
-        Ok(_) => println!("Connected!"),
-        Err(e) => println!("Error {}", e)
+    if matches.is_present("disconnect") {
+        match device.disconnect() {
+            Ok(_) => println!("Disconnected!"),
+            Err(e) => println!("Error {}", e),
+        }
+    } else {
+        match device.connect(5_000) {
+            Ok(_) => println!("Connected!"),
+            Err(e) => println!("Error {}", e),
+        }
     }
 }
